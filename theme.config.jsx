d08e3e7b5612fc7678b03270c02e8d4ed11759a1
Nick Thompson <ncthom91@gmail.com>
2023-09-29 08:41:51 -0400

    Better page layout

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,9 @@ export default {
   logo: <span>My Nextra Documentation</span>,
   project: {
     link: 'https://github.com/shuding/nextra'
-  }
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
   // ... other theme options
 }
