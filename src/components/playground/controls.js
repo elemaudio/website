39122960755fdda17a133941d94fc750efd233af
Nick Thompson <ncthom91@gmail.com>
2023-10-05 09:06:44 -0400

    Controls looking good

--- src/components/playground/controls.js
+++ src/components/playground/controls.js
@@ -1,11 +1,8 @@
 import {
   PlayIcon,
   ArrowPathIcon,
-  MinusCircleIcon,
-  XCircleIcon,
-  SpeakerXMarkIcon,
   ArrowUpOnSquareIcon,
-  CheckCircleIcon,
+  CommandLineIcon,
 } from '@heroicons/react/24/outline';
 
 
@@ -13,30 +10,38 @@ function Button(props) {
   return (
     <button
       {...props}
-      className="flex items-center px-2 py-2 mr-4 border rounded-full">
+      className="flex items-center px-2 py-2 mr-4 border rounded-full hover:border-pink-300 dark:hover:border-pink-400 active:bg-pink-50 dark:active:bg-gray-700">
       {props.children}
     </button>
   );
 }
 
+export function Controls(props) {
+  const {
+    onPlayPause,
+    onRestart,
+    onShare,
+    statusMessage,
+  } = props;
 
-export function Controls() {
   return (
     <div className="h-full flex">
-      <div className="flex-1 flex items-center">
-        <Button className="items-center px-2 py-2">
+      <div className="flex-1 flex items-center px-5">
+        <Button onClick={onPlayPause}>
           <PlayIcon className="h-5 w-5" />
         </Button>
-        <Button className="items-center px-2 py-2 mr-2">
-          <SpeakerXMarkIcon className="h-5 w-5" />
+        <Button onClick={onRestart}>
+          <ArrowPathIcon className="h-5 w-5" />
         </Button>
-        <Button className="items-center px-2 py-2">
+        <Button onClick={onShare}>
           <ArrowUpOnSquareIcon className="h-5 w-5" />
         </Button>
       </div>
-      <div className="flex-1 flex items-center">
-      </div>
-      <div>
+      <div className="flex-1 flex items-center px-5">
+        <div className="flex items-center py-2">
+          <CommandLineIcon className="h-5 w-5" />
+          <span className="px-2">Ok!</span>
+        </div>
       </div>
     </div>
   );
