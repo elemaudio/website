fd1226979878d10a9896e3ba6fdb05f246949bba
Nick Thompson <ncthom91@gmail.com>
2023-10-06 09:10:08 -0400

    Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
 import '../styles/globals.css';
 
+import Head from 'next/head'
 import { RenderContextProvider } from '@/lib/RenderContext';
 
 
 export default function App({ Component, pageProps }) {
   return (
-    <RenderContextProvider>
-      <Component {...pageProps} />
-    </RenderContextProvider>
+    <>
+      <Head>
+        <script defer data-domain="elementary.audio" src="https://plausible.io/js/script.js"></script>
+      </Head>
+      <RenderContextProvider>
+        <Component {...pageProps} />
+      </RenderContextProvider>
+    </>
   );
 }
